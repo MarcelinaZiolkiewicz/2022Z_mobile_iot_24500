6fd639cc67c7dcffd60d446dac6ab701855b5a37
Michau <m.ziolkiewicz1@gmail.com>
2022-12-18 15:12:38 +0100

    Added lab 2 files

---
+++ mobile_iot_lab02/Lab2/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace Lab2;
+
+public partial class MainPage : ContentPage
+{
+	string translateNumber;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnTranslate(object sender, EventArgs e)
+	{
+		string enteredNumber = PhonenumberText.Text;
+		translateNumber = Lab2.PhonewordTranslator.ToNumber(enteredNumber);
+
+		if (!string.IsNullOrEmpty(translateNumber))
+		{
+			CallButton.IsEnabled = true;
+			CallButton.Text = "Call " + translateNumber;
+		} else
+		{
+			CallButton.IsEnabled = false;
+			CallButton.Text = "Call";
+		}
+	}
+
+	async void OnCall(object sender, System.EventArgs e)
+	{
+		if(await this.DisplayAlert(
+			"Dial a Number", "Would you like to call " + translateNumber + "?", "Yes", "No"))
+		{
+			try
+			{
+				if (PhoneDialer.Default.IsSupported) PhoneDialer.Default.Open(translateNumber);
+			}
+			catch (ArgumentNullException)
+			{
+				await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+			}
+			catch (Exception)
+			{
+				await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+			}
+		}
+	}
+
+}
+

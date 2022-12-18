b519633cfa297005ab3bf827525a84177cc2bce7
Michau <m.ziolkiewicz1@gmail.com>
2022-12-18 17:32:33 +0100

    Added lab 4

---
+++ mobile_iot_lab04/mobile_iot_lab04/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace mobile_iot_lab04;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ mobile_iot_lab04/mobile_iot_lab04/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace mobile_iot_lab04;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
